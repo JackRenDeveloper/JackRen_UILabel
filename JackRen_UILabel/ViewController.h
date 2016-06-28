84e14f9a4e070525f9c0fbba3bf97463922004f1
JackRenDeveloper <jackren_developer@163.com>
2016-06-28 15:21:22 -0700

    Initial Commit

---
+++ JackRen_UILabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JackRen_UILabel
+//
+//  Created by JackRen on 16/6/28.
+//  Copyright © 2016年 JackRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JackRen_UILabel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JackRen_UILabel
+//
+//  Created by JackRen on 16/6/28.
+//  Copyright © 2016年 JackRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
